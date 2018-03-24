7eebaa0c723b630e4be29bbde85d9752ab39de13
Craig Steele <cr@igsteele.com>
2018-03-24 18:33:02 +0000

    pep8 changes

--- poem.py
+++ poem.py
@@ -24,9 +24,13 @@ line20 = "A blethering, blustering, drunken blellum;"
 line21 = "That frae November till October,"
 line22 = "Ae market-day thou was nae sober,"
 line23 = "That ilka melder, wi' the miller"
- 
 
-lines = [line, line2, line3, line4, line5, line6, line7, line8, line9, line10, line11, line12, line13, line14, line15, line16, line17, line18, line19, line20, line21, line23, line23]
+lines = [line, line2, line3, line4,
+         line5, line6, line7, line8,
+         line9, line10, line11, line12,
+         line13, line14, line15, line16,
+         line17, line18, line19, line20,
+         line21, line22, line23]
 
 for line in lines:
     words = line.split(" ")
@@ -39,6 +43,3 @@ for line in lines:
     space = " "
     changed_line = space.join(words)
     print(changed_line)
-
-    
-
